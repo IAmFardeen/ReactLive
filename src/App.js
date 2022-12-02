a4d964dd730c848d929e848e457c33f16ebe16b4
IAmFardeen <khan.fardeen17091998@gmail.com>
2022-12-02 16:00:08 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+
+import FirstComponent from "./component/FirstComponent"
+import './App.css';
+
+function App() {
+  
+
+return(
+  <div >
+      <FirstComponent/>
+  </div>
+)
+
+}
+
+export default App;
